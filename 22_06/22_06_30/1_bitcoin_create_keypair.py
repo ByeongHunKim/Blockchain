e8f7e9e29dc8ce65357d14507df6a1c7364c3896
ByeongHunKim <rkwhr912@naver.com>
2022-06-30 11:36:15 +0900

    #11 bitcoin 라이브러리 사용한 지갑생성 연습

---
+++ 22_06/22_06_30/1_bitcoin_create_keypair.py
@@ -0,0 +1,19 @@
+# pip install bitcoin
+
+# import bitcoin
+from bitcoin import *
+
+# Create Private Key -> 0088bd01802f36675d341f7d59419400b40822
+private_key = random_key()
+print("1. 생성된 private_key : ",private_key)
+
+# Create Public Key -> 04c1739c087971238e461e017b9ee2326daad02699648c9be4bf1d418b93e34787b2153455d8
+public_key = privtopub(private_key)
+print("2. 생성된 public_key : ",public_key)
+
+# Create A Bitcoin Address
+address = pubtoaddr(public_key)
+print("3. 생성된 bitcoin address : ",address)
+# 1Bdz58SG4zqmtf6xM4HR8WbKj5tXG9NrxP
+
+# 첫번째로 생성한 address 조회 -> https://blockchair.com/search?q=1PRz1q3npXLTbLCs4gg5YCJZXVMaWX5CjB&submitButton=
