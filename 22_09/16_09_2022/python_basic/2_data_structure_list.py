9414655b93118c149e1c384904bc302d23053b9d
ByeongHunKim <rkwhr912@naver.com>
2022-09-16 17:40:13 +0900

    #27 python_basic

--- 22_09/16_09_2022/python_basic/2_data_structure_list.py
+++ 22_09/16_09_2022/python_basic/2_data_structure_list.py
@@ -1,3 +1,19 @@
 days_of_week = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
 
 print(days_of_week)
+
+# list(리스트의 장점)
+
+
+print(dir(days_of_week))
+"""
+['__add__', '__class__', '__contains__', '__delattr__', 
+'__delitem__', '__dir__', '__doc__', '__eq__', '__format__',
+'__ge__', '__getattribute__', '__getitem__', '__gt__', '__hash__',
+'__iadd__', '__imul__', '__init__', '__init_subclass__', '__iter__', 
+'__le__', '__len__', '__lt__', '__mul__', '__ne__', '__new__',
+'__reduce__', '__reduce_ex__', '__repr__', '__reversed__',
+'__rmul__', '__setattr__', '__setitem__', '__sizeof__', '__str__', 
+'__subclasshook__', 'append', 'clear', 'copy', 'count', 'extend', 
+'index', 'insert', 'pop', 'remove', 'reverse', 'sort']
+"""
