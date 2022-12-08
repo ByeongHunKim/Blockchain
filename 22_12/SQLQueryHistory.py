37c6ae3ab88ba316d119d7bd6376d8f4ff3be011
ByeongHunKim <rkwhr912@naver.com>
2022-12-08 17:00:07 +0900

    :white_check_mark: confirmed : 12월 8일 수요일

--- 22_12/SQLQueryHistory.py
+++ 22_12/SQLQueryHistory.py
@@ -7,6 +7,7 @@ SELECT username FROM blog_signup WHERE username ="G-chlrjs23@gmail.com"
 
 UPDATE blog_signup SET username = REPLACE(username, 'G-', "")
 
+UPDATE blog_signup SET BtcValue = "0" WHERE BtcValue IS NULL;
 
 # mongoDB
 mongorestore -d name bson경로
